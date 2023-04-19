8c0681366b5fb47785f465e216f479a954686278
Hyperion <radoslaw.lewandowski10@gmail.com>
2023-04-19 22:08:38 +0200

    Add NextJS Image component

--- app/about/Movie.jsx
+++ app/about/Movie.jsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Movie({ title, id, poster_path, release_date }) {
+  const imagePath = "https://image.tmdb.org/t/p/w500";
+  console.log(poster_path);
   return (
     <div>
       <h1>{title}</h1>
       <h2>{release_date}</h2>
       <Link href={`/test`}>
-        <img src="" alt="" />
+        <Image
+          src={imagePath + poster_path}
+          width={800}
+          height={800}
+          alt={title}
+        />
       </Link>
     </div>
   );
