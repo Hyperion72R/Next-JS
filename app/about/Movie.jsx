884061c3b2fb46dd6bd094db2e462a29000a0afb
Hyperion <radoslaw.lewandowski10@gmail.com>
2023-04-19 22:57:20 +0200

    Add Img props for the better img quality

--- app/about/Movie.jsx
+++ app/about/Movie.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 export default function Movie({ title, id, poster_path, release_date }) {
-  const imagePath = "https://image.tmdb.org/t/p/w500";
+  const imagePath = "https://image.tmdb.org/t/p/original";
   console.log(poster_path);
   return (
     <div>
@@ -10,9 +10,12 @@ export default function Movie({ title, id, poster_path, release_date }) {
       <Link href={`/test`}>
         <Image
           src={imagePath + poster_path}
-          width={800}
-          height={800}
+          width={600}
+          height={600}
           alt={title}
+          quality={100}
+          unoptimized={true}
+          priority
         />
       </Link>
     </div>
